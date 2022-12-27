8616247bb567fc4e0bf1c767b28beb76a1fb1a2c
haseenavamsi <121420151+haseenavamsi@users.noreply.github.com>
2022-12-27 14:49:59 +0530

    Add files via upload

---
+++ Dom6.js
@@ -0,0 +1,12 @@
+> var titles = document.querySelectorAll('li');
+
+console.log(titles);
+titles[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even= document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length; i++){
+  odd[i].style.backgroundColor = 'green';
+  
+ }
